9ca86cd6ca71bbcdc26c1d6dc6efd58d4bff642b
Gage Norton <nortongage0@gmail.com>
2023-05-16 18:21:28 -0500

    Add improvemnts

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const downloadBtn = document.getElementById('download-btn')
+const downloadBtn = document.getElementById('download-btn');
 const colorInput = document.getElementById('color');
 const colorSwatches = document.getElementsByClassName('color-swatch');
 const sizeInput = document.getElementById('size');
@@ -8,7 +8,7 @@ const shapeInput = document.getElementById('shape');
 const eraseButton = document.getElementById('erase');
 const eraseAllButton = document.getElementById('erase-all');
 const fileName = document.getElementById('file-name');
-const exampleModal = document.getElementById('exampleModal');
+const undoButton = document.getElementById('undo');
 
 const colorPalette = ['#000000', '#ff0000', '#00ff00', '#0000ff', '#ffff00'];
 
@@ -19,7 +19,7 @@ let eraser = false;
 
 let color = colorPalette[0]; // Set initial color to the first color in the palette
 
-// saves the color, shape, size from the previous step 
+// saves the color, shape, size from the previous step
 let previousColor = color;
 let previousSize = size;
 let previousShape = shape;
@@ -29,7 +29,19 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
-// event listeners for color input (box) and color swatches (pre- determined buttons)
+// Stack to store drawn elements for undo functionality
+const drawnElements = [];
+
+// Load saved drawn elements from localStorage on page load
+window.addEventListener('load', () => {
+    const savedElements = localStorage.getItem('drawnElements');
+    if (savedElements) {
+        drawnElements.push(...JSON.parse(savedElements));
+        redrawElements();
+    }
+});
+
+// event listeners for color input (box) and color swatches (pre-determined buttons)
 colorInput.addEventListener('input', () => {
     color = colorInput.value;
 });
@@ -41,56 +53,118 @@ for (let i = 0; i < colorSwatches.length; i++) {
     });
 }
 
+// pen size 
 sizeInput.addEventListener('input', () => {
     previousSize = size;
     size = sizeInput.value;
 });
 
+// round or square
 shapeInput.addEventListener('input', () => {
     previousShape = shape;
     shape = shapeInput.value;
 });
 
 // Set event listener for eraser button
-eraseButton.addEventListener('click', eraseButtonFunction);
-
-function eraseButtonFunction() {
+eraseButton.addEventListener('click', () => {
     eraser = !eraser;
     if (eraser) {
         previousColor = color;
-        shape = 'square';
         color = '#ffffff';
-        eraseButton.textContent = "Draw"
+        eraseButton.textContent = 'Draw';
     } else {
         color = previousColor;
-        eraseButton.textContent = "Erase"
+        eraseButton.textContent = 'Erase';
     }
-}
+});
 
 // Set event listener for erase all button
 eraseAllButton.addEventListener('click', () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    eraseButtonFunction()
+    eraseButtonFunction();
+    clearSavedElements();
 });
 
-// TODO: It only saves the the lines but not the white background so the images are transparent. 
+// TODO: It only saves the lines but not the white background so the images are transparent.
+// downloadBtn.addEventListener('click', () => {
+//     const link = document.createElement('a');
+//     const newName = fileName.value + '.png';
+//     link.download = newName;
+//     link.href = canvas.toDataURL('image/png');
+//     link.click();
+// });
 downloadBtn.addEventListener('click', () => {
     const link = document.createElement('a');
-    const newName = fileName.value + '.png'
-    link.download = newName
-    link.href = canvas.toDataURL('image/png');
+    const newName = fileName.value + '.png';
+
+    const downloadCanvas = document.createElement('canvas');
+    const downloadContext = downloadCanvas.getContext('2d');
+
+    // Set the same width and height as the original canvas
+    downloadCanvas.width = canvas.width;
+    downloadCanvas.height = canvas.height;
+
+    // Set the background color
+    downloadContext.fillStyle = '#ffffff';
+    downloadContext.fillRect(0, 0, downloadCanvas.width, downloadCanvas.height);
+
+    // Draw the existing canvas onto the download canvas
+    downloadContext.drawImage(canvas, 0, 0);
+
+    link.download = newName;
+    link.href = downloadCanvas.toDataURL('image/png');
     link.click();
 });
 
-exampleModal.addEventListener('show.bs.modal', function (event) {
-})
+// Event listener for undo button
+undoButton.addEventListener('click', () => {
+    undoDraw();
+});
+
+// Undo draw function 
+function undoDraw() {
+    if (drawnElements.length > 0) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawnElements.pop();
+        redrawElements();
+        saveElements();
+    }
+};
+
+function redrawElements() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.globalCompositeOperation = 'source-over';
+    drawnElements.forEach((element) => {
+        context.strokeStyle = element.color;
+        context.lineWidth = element.size;
+        context.lineJoin = element.shape;
+        context.lineCap = element.shape;
+        context.beginPath();
+        context.moveTo(element.startX, element.startY);
+        context.lineTo(element.endX, element.endY);
+        context.stroke();
+    });
+};
+
+// clears local storage of the old drawing
+function clearSavedElements() {
+    localStorage.removeItem('drawnElements');
+}
+
+// Save drawn elements to localStorage
+function saveElements() {
+    localStorage.setItem('drawnElements', JSON.stringify(drawnElements));
+};
 
 // Draw functions
 function draw(e) {
     if (!isDrawing) return;
 
     if (eraser) {
-        context.globalCompositeOperation = 'destination-out';
+        // context.globalCompositeOperation = 'destination-out';
+        context.globalCompositeOperation = 'source-over';
+        context.strokeStyle = '#ffffff';
+        context.lineWidth = size;
     } else {
         context.globalCompositeOperation = 'source-over';
         context.strokeStyle = color;
@@ -103,6 +177,20 @@ function draw(e) {
     context.moveTo(lastX, lastY);
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke();
+
+    const element = {
+        color: context.strokeStyle,
+        size: context.lineWidth,
+        shape: context.lineJoin,
+        startX: lastX,
+        startY: lastY,
+        endX: e.offsetX,
+        endY: e.offsetY,
+    };
+
+    drawnElements.push(element);
+    saveElements();
+
     [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
@@ -114,3 +202,6 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => (isDrawing = false));
 canvas.addEventListener('mouseout', () => (isDrawing = false));
+
+
+
